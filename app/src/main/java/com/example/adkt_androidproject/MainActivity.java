6062481670f5369ccbe5d0949762a083c26e990a
Phand <phanduc09052000@gmail.com>
2022-01-04 10:13:23 +0700

    Login - Registration - Add Image form Photo Library and show to activity

--- app/src/main/java/com/example/adkt_androidproject/MainActivity.java
+++ app/src/main/java/com/example/adkt_androidproject/MainActivity.java
@@ -2,18 +2,32 @@ package com.example.adkt_androidproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView tvCreateAccount;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //hello
-        //hello 23
-        //Hello Đức nè hố hố
-        //An hello
-        //Rảnh quá hello
+
+        tvCreateAccount = findViewById(R.id.tvCreateAccount);
+
+        tvCreateAccount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openCreateAccount();
+            }
+        });
+
+    }
+
+    private void openCreateAccount() {
+        Intent intent = new Intent(MainActivity.this, CreateAccountActivity.class);
+        startActivity(intent);
     }
 }
