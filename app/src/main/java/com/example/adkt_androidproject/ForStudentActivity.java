c7545f832dc620e56bdba72b93c08ae960b0b887
Kien <ntkien400@gmail.com>
2022-01-08 20:38:26 +0700

    Thêm giao diện subject, info cho student

--- app/src/main/java/com/example/adkt_androidproject/ForStudentActivity.java
+++ app/src/main/java/com/example/adkt_androidproject/ForStudentActivity.java
@@ -1,14 +1,41 @@
 package com.example.adkt_androidproject;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class ForStudentActivity extends AppCompatActivity {
-
+    CardView cvSubject,cvInfo;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_for_student);
+        cvSubject = findViewById(R.id.cvSubject);
+        cvSubject.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openSubject();
+            }
+        });
+        cvInfo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openInfo();
+            }
+        });
+
+    }
+
+    private void openInfo() {
+        Intent intent = new Intent(ForStudentActivity.this, StudentInfoActivity.class);
+        startActivity(intent);
+    }
+
+    private void openSubject() {
+        Intent intent = new Intent(ForStudentActivity.this, StudentSubjectActivity.class);
+        startActivity(intent);
     }
 }
