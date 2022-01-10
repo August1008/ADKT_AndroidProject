bacc0f5f8b84b849287add3ffa5df0c6c9698b9d
Phand <phanduc09052000@gmail.com>
2022-01-10 21:40:37 +0700

    Làm chức năng trên ForStudentActivity, giao diện tham gia lớp học

---
+++ app/src/main/java/com/example/adkt_androidproject/ClassAdapter.java
@@ -0,0 +1,90 @@
+package com.example.adkt_androidproject;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class ClassAdapter extends RecyclerView.Adapter<ClassAdapter.ClassViewHolder>{
+    private List<String> list;
+
+    public ClassAdapter(List<String> list) {
+        this.list = list;
+        notifyDataSetChanged();
+    }
+    @NonNull
+    @Override
+    public ClassViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_class, parent, false);
+        return new ClassAdapter.ClassViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ClassViewHolder holder, int position) {
+        String str = list.get(position);
+        if (str == null) {
+            return;
+        }
+        holder.tvTimes.setText("09/05/2000 - 12:30");
+        holder.tvTeacherName.setText("Tên giảng viên");
+        holder.tvClassname.setText(str);
+        holder.layout.setCardBackgroundColor(holder.itemView.getResources().getColor(randomColor(), null));
+    }
+
+    @Override
+    public int getItemCount() {
+        if (list == null) {
+            return 0;
+        } else {
+            return list.size();
+        }
+    }
+
+    public class ClassViewHolder extends RecyclerView.ViewHolder {
+
+        TextView tvClassname, tvTeacherName, tvTimes;
+        CardView layout;
+        public ClassViewHolder(@NonNull View itemView) {
+            super(itemView);
+            layout = itemView.findViewById(R.id.item_class);
+            tvClassname = itemView.findViewById(R.id.tvClassname);
+            tvTeacherName = itemView.findViewById(R.id.tvTeacherName);
+            tvTimes = itemView.findViewById(R.id.tvTimes);
+        }
+    }
+
+    private int randomColor() {
+        List<Integer> colorCode = new ArrayList<>();
+        colorCode.add(R.color.red);
+        colorCode.add(R.color.pink);
+        colorCode.add(R.color.purple);
+        colorCode.add(R.color.deep_purple);
+        colorCode.add(R.color.indigo);
+        colorCode.add(R.color.blue);
+        colorCode.add(R.color.light_blue);
+        colorCode.add(R.color.cyan);
+        colorCode.add(R.color.teal);
+        colorCode.add(R.color.green);
+        colorCode.add(R.color.light_green);
+        colorCode.add(R.color.lime);
+        colorCode.add(R.color.yellow);
+        colorCode.add(R.color.amber);
+        colorCode.add(R.color.orange);
+        colorCode.add(R.color.deep_orange);
+        colorCode.add(R.color.brown);
+        colorCode.add(R.color.gray);
+        colorCode.add(R.color.blue_gray);
+
+        Random random = new Random();
+        int number = random.nextInt(colorCode.size());
+        return colorCode.get(number);
+    }
+}
--- app/src/main/java/com/example/adkt_androidproject/ForStudentActivity.java
+++ app/src/main/java/com/example/adkt_androidproject/ForStudentActivity.java
@@ -1,25 +1,53 @@
 package com.example.adkt_androidproject;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.cardview.widget.CardView;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageButton;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ForStudentActivity extends AppCompatActivity {
 
-    CardView cvLogout;
+    ImageButton ibLog_out;
+    ClassAdapter classAdapter;
+    RecyclerView rvClass;
+    List<String> list;
+    FloatingActionButton fabAdd;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_for_student);
 
-        cvLogout = findViewById(R.id.cvLogout);
-        cvLogout.setOnClickListener(new View.OnClickListener() {
+        rvClass = findViewById(R.id.rvClass);
+        ibLog_out = findViewById(R.id.ibLog_out);
+        fabAdd = findViewById(R.id.fabAdd);
+
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+        rvClass.setLayoutManager(linearLayoutManager);
+        list = new ArrayList<>();
+        addList();
+        classAdapter = new ClassAdapter(list);
+        rvClass.setAdapter(classAdapter);
+        ibLog_out.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 startMainActivity();
+            }
+        });
+
+        fabAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startAddClassActivity();
             }
         });
     }
@@ -28,5 +56,18 @@ public class ForStudentActivity extends AppCompatActivity {
         Intent intent = new Intent(this, MainActivity.class);
         intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK|Intent.FLAG_ACTIVITY_CLEAR_TASK);
         startActivity(intent);
+    }
+    private void startAddClassActivity() {
+        Intent intent = new Intent(this, JoinClassActivity.class);
+        startActivity(intent);
+    }
+
+    public void addList() {
+        list.add("19DTHA1");
+        list.add("19DTHA2");
+        list.add("19DTHA5");
+        list.add("19DTHA6");
+        list.add("19DTHA8");
+        list.add("19DTHA10");
     }
 }
